f5ce89399e675a84f57709a20ec805788507be84
Marco Antonio Rulfo Castro <81320236+Marcor360@users.noreply.github.com>
2025-04-14 17:06:02 -0600

    Update

--- BD/Base1.sql
+++ BD/Base1.sql
@@ -450,4 +450,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-14 15:35:26
+-- Dump completed on 2025-04-14 15:35:43
